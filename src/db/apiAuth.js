89c00ced83d8432dec6861737891a90d6e767c6d
Utsav <utssavvpatel@gmail.com>
2025-04-06 18:28:28 +0530

    Signup API

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
     const { data, error } = await supabase.auth.signInWithPassword({
@@ -12,8 +12,31 @@ export async function login({ email, password }) {
 }
 
 export async function getCurrentUser() {
-    const {data: session, error} = await supabase.auth.getSession();
-    if(!session.session) return null;
-    if(error) throw new Error(error.message);
+    const { data: session, error } = await supabase.auth.getSession();
+    if (!session.session) return null;
+    if (error) throw new Error(error.message);
     return session.session?.user;
+}
+
+export async function signup({ name, email, password, profile_pic }) {
+    const fileName = `dp-${name.split(" ").join("-")}-${Math.random()}`;
+    const { error: storageError } = await supabase.storage
+        .from("profile-pic")
+        .upload(fileName, profile_pic);
+
+    if (storageError) throw new Error(storageError.message);
+
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                name,
+                profile_pic: `${supabaseUrl}/storage/v1/object/public/profile-pic/${fileName}`,
+            },
+        },
+    });
+
+    if (error) throw new Error(error.message);
+    return data;
 }
