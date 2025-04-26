3807165c71558041c3b1e1d4ad7babdb95120d5e
Utsav <utssavvpatel@gmail.com>
2025-04-26 18:11:06 +0530

    Minor ChangesI

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -61,3 +61,17 @@ export async function createUrl(
 
     return data;
 }
+
+export async function getLongUrl(id) {
+    const { data, error } = await supabase
+        .from("urls")
+        .select("id, original_url")
+        .or(`short_url.eq.${id}, custom_url.eq.${id}`)
+        .single();
+    if (error) {
+        console.error(error.message);
+        throw new Error("Error fetching short URL");
+    }
+
+    return data;
+}
