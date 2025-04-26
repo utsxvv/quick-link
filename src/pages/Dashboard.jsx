50a945e8881ab31d129cbf96ea2d8f9c1b208b88
Utsav <utssavvpatel@gmail.com>
2025-04-26 14:23:19 +0530

    create url dialog

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import CreateLink from "@/components/create-link";
 import Error from "@/components/Error";
 import LinkCard from "@/components/link-card";
 import { Button } from "@/components/ui/button";
@@ -72,7 +73,7 @@ const Dashboard = () => {
 
             <div className="flex justify-between">
                 <h1 className="text-4xl font-extrabold">My Links</h1>
-                <Button>Create Link</Button>
+                <CreateLink />
             </div>
 
             <div className="relative">
