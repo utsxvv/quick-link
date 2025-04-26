1636898d6a9437a9cd3abcee0299f3c30d2bca5e
Utsav <utssavvpatel@gmail.com>
2025-04-26 18:45:08 +0530

    redirection logic

--- src/pages/RedirectLink.jsx
+++ src/pages/RedirectLink.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { getLongUrl, storeClicks } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const RedirectLink = () => {
-    return <div>Redirect Link</div>;
+    const { id } = useParams();
+
+    const { loading, data, fn } = useFetch(getLongUrl, id);
+
+    const { loading: loadingStats, fn: fnStats } = useFetch(storeClicks, {
+        id: data?.id,
+        originalUrl: data?.original_url,
+    });
+
+    useEffect(() => {
+        fn();
+    }, []);
+
+    useEffect(() => {
+        if (!loading && data) {
+            fnStats();
+        }
+    }, [loading]);
+
+    if (loading || loadingStats) {
+        return (
+            <>
+                <BarLoader width={"100%"} color="#1E2939" />
+                <br />
+                Redirecting...
+            </>
+        );
+    }
+
+    return null;
 };
 
 export default RedirectLink;
