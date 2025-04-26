3e456d6ecc734557c6dec99dc25804401e1791c6
Utsav <utssavvpatel@gmail.com>
2025-04-26 18:27:19 +0530

    Statistics API

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
@@ -75,3 +76,26 @@ export async function getLongUrl(id) {
 
     return data;
 }
+
+const parser = new UAParser();
+
+export const storeClicks = async ({ id, originalUrl }) => {
+    try {
+        const res = parser.getResult();
+        const device = res.type || "desktop";
+
+        const response = await fetch("https://ipapi.co/json");
+        const { city, country_name: country } = await response.json();
+
+        await supabase.from("clicks").insert({
+            url_id: id,
+            device: device,
+            city: city,
+            country: country,
+        });
+
+        window.location.href = originalUrl;
+    } catch (error) {
+        console.error("Error recording click:", error);
+    }
+};
