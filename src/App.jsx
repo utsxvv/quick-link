468bb87e99a27a56fd24d358fe7f5a609d80385a
Utsav <utssavvpatel@gmail.com>
2025-04-30 18:08:20 +0530

    minor bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import RedirectLink from "./pages/RedirectLink";
 import UrlProvider from "./context";
 import RequireAuth from "./components/require-auth";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const router = createBrowserRouter([
     {
